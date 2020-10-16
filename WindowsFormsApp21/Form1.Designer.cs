786e6b8abe1524eb7288d5741a64dfe164e91f13
Sl1ed <45339767+Sl1ed@users.noreply.github.com>
2020-10-16 21:16:21 +0300

    орлорл

--- WindowsFormsApp21/Form1.Designer.cs
+++ WindowsFormsApp21/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.зановоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -98,11 +99,22 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(432, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1184, 561);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -125,6 +137,7 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ToolStripMenuItem информацияОРазработчикеToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem зановоToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsFormsApp21/Form1.cs
+++ WindowsFormsApp21/Form1.cs
@@ -13,45 +13,58 @@ namespace WindowsFormsApp21
 {
     public partial class Form1 : Form
     {
-    
+
         public delegate void AsyncMove();
         Ball[] redball = new Ball[2];
         Ball[] blueball = new Ball[2];
+        private CancellationTokenSource cancelToken = new CancellationTokenSource();
         public Form1()
         {
             for (int i = 0; i < 2; i++)
             {
-                redball[i] = new Ball(new Point(0, ((i*200) + 100)), "Right", true, Brushes.Red);
+                redball[i] = new Ball(new Point(0, ((i * 200) + 100)), "Right", true, Brushes.Red);
             }
             for (int i = 0; i < 2; i++)
             {
-                blueball[i] = new Ball(new Point(1085, (i*200)+ 200), "Left", true, Brushes.Blue);
+                blueball[i] = new Ball(new Point(1085, (i * 200) + 200), "Left", true, Brushes.Blue);
                 redball[0].ChangeDirection += new AsyncMove(blueball[i].Nazad);
             }
             InitializeComponent();
+            DoubleBuffered = true;
             for (int i = 0; i < 2; i++)
             {
                 WaitCallback del = new WaitCallback(redball[i].RightMove); // инкапсуляция функции
-                ThreadPool.QueueUserWorkItem(del);// запрос потока из пула потоков для выполнения функции
+                ThreadPool.QueueUserWorkItem(del); // запрос потока из пула потоков для выполнения функции
+            }
+                ParallelOptions partOpts = new ParallelOptions();
+                partOpts.CancellationToken = cancelToken.Token;
                 Task.Factory.StartNew(() =>
                 {
+
                     Parallel.For(0, 2, cur =>
                     {
-                        blueball[cur].LeftMove();
-                        Invoke((Action)delegate
+                        try
                         {
-                            Text = string.Format("Поток - {0}", cur);
-                        });
+                            blueball[cur].LeftMove(partOpts);
+                            Invoke((Action)delegate
+                            {
+                                Text = string.Format("Поток - {0}", cur);
+                            });
+                        }
+                        catch (OperationCanceledException ex)
+                        {
+                            Invoke((Action)delegate
+                            {
+                                Text = string.Format("{0}", ex.Message);
+                            });
+                        }
                     });
                 });
+
             }
-            DoubleBuffered = true;
-        }
 
 
-
-
-    private void timer1_Tick(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
             timer1.Enabled = true;
             Invalidate();
@@ -97,15 +110,25 @@ namespace WindowsFormsApp21
                 context.FillEllipse(this.color, new Rectangle(this.position, this.size));
             }
             bool live = true;
-            public void LeftMove()
+            public void LeftMove(ParallelOptions obj)
             {
+
                 while (live)
                 {
-                    if (bluestart)
-                        position.X -= 10;
-                    Thread.Sleep(100);
-                    if (position.X <= 30)
+                    try
                     {
+                        obj.CancellationToken.ThrowIfCancellationRequested();
+                        if (bluestart)
+                            position.X -= 10;
+                        Thread.Sleep(100);
+                        if (position.X <= 30)
+                        {
+                            live = false;
+                        }
+                    }
+                    catch(OperationCanceledException ex)
+                    {
+                        MessageBox.Show(string.Format("{0}", ex.Message));
                         live = false;
                     }
                 }
@@ -167,5 +190,9 @@ namespace WindowsFormsApp21
             Application.Restart();
         }
 
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            cancelToken.Cancel();
+        }
     }
 }
